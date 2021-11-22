396333516db93c39938c2ca189e7093eb65595e2
vochihieu <chihieu8320@gmail.com>
2021-11-22 14:30:00 +0700

    delete aggregation

--- src/controller/aggrerate.controller.ts
+++ src/controller/aggrerate.controller.ts
@@ -7,50 +7,8 @@ class AggrerateController
 {
     async Aggrerate(req: any, res: any)
     {
-        const {userID, skip} = req.query;
-        if(userID && skip)
-        {
-            const check_user = await ChannelUser.findOne({userID: userID});
-            if(check_user)
-            {
-                const result = await ChannelUser.aggregate([
-                    {
-                        $match: {userID: userID}
-                    },
-                    { "$addFields": { "userChannelId": { "$toObjectId": "$channelID" }}},
-                    {
-                        $lookup:
-                        {
-                           
-                            from: "chanels",
-                            localField:"userChannelId",
-                            foreignField: "_id",
-                            as: "channels"
-                        }
-                    },
-                    {
-                        $skip: +skip
-                    },
-                    {
-                        $limit: 5
-                    },
-                    {
-                        $sort: {"updatedAt": -1}
-                    }
-                ])
         
-                res.json(result);    
-            }
-            else
-            {
-                res.sendStatus(400);
-            }
-            
-        }
-        else
-        {
-            res.sendStatus(400);
-        }
+       
 
        
     }
